48004f3ca11f1fa0f2ba6c12a5d3c379110c12e0
dmm22 <davidmartin71090@gmail.com>
2023-06-25 11:48:33 -0400

    Add Button component and tweak SidePanel styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import ColorSwatches from "./components/ColorSwatches"
 import NoteEditor from "./components/NoteEditor"
-import SavedNotes from "./components/SavedNotes"
 import SidePanel from "./components/SidePanel"
 import NoteContextProvider from "./contexts/NoteContext"
 
--- src/components/SidePanel.tsx
+++ src/components/SidePanel.tsx
@@ -1,9 +1,22 @@
+import { useContext } from "react"
+import Button from "./Button"
 import SavedNotes from "./SavedNotes"
+import { NoteContext } from "../contexts/NoteContext"
 
 export default function SidePanel() {
+  const { addNote } = useContext(NoteContext)
   return (
-    <aside className="w-1/6 text-text border border-secondaryBg1 p-3">
-      <SavedNotes />
+    <aside className="w-1/5 p-3 text-text border border-secondaryBg1 flex flex-col justify-between">
+      <div className="mb-auto">
+        <input className="w-full" />
+        <SavedNotes />
+      </div>
+      <Button
+        text="New Note"
+        clickHandler={addNote}
+        color="text-text"
+        background="bg-primaryBtn"
+      />
     </aside>
   )
 }
