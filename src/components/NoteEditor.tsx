b3dff31cd6dde123edc05e3966b477850bfc8fb5
dmm22 <davidmartin71090@gmail.com>
2023-06-24 20:08:10 -0400

    Add function to save notes

--- src/components/NoteEditor.tsx
+++ src/components/NoteEditor.tsx
@@ -2,11 +2,13 @@ import { useContext } from "react"
 import { NoteContext } from "../contexts/NoteContext"
 
 export default function NoteEditor() {
-  const { currentNote, editCurrentNote } = useContext(NoteContext)
+  const { currentNote, editCurrentNote, saveCurrentNote, notes } =
+    useContext(NoteContext)
   const { title, body } = currentNote
 
   return (
     <section className="text-text flex flex-col">
+      <button onClick={saveCurrentNote}>Save</button>
       <input
         onChange={e => editCurrentNote("title", e.target.value)}
         value={title}
@@ -17,6 +19,7 @@ export default function NoteEditor() {
         value={body}
         className="bg-bg resize-none border-secondaryBg1 border-2"
       ></textarea>
+      <pre>{JSON.stringify(notes, null, 2)}</pre>
     </section>
   )
 }
--- src/contexts/NoteContext.tsx
+++ src/contexts/NoteContext.tsx
@@ -2,8 +2,10 @@ import { createContext, useState } from "react"
 import Note from "../types/Note"
 
 interface NoteContextInterface {
+  note: Note[]
   currentNote: Note
   editCurrentNote: (property: "title" | "body", newValue: string) => void
+  saveCurrentNote: () => void
 }
 
 export const NoteContext = createContext<NoteContextInterface>(
@@ -22,17 +24,40 @@ export default function NoteContextProvider({
 }: {
   children: JSX.Element
 }) {
+  const [notes, setNotes] = useState<Note[]>([])
   const [currentNote, setCurrentNote] = useState(blankNote())
 
   function editCurrentNote(property: "title" | "body", newValue: string) {
     setCurrentNote(currentNote => ({ ...currentNote, [property]: newValue }))
   }
 
+  function saveCurrentNote() {
+    const existingNoteIndex = getCurrentNoteIndex()
+
+    if (existingNoteIndex >= 0) {
+      setNotes(notes => [
+        currentNote,
+        ...notes.slice(0, existingNoteIndex),
+        ...notes.slice(existingNoteIndex + 1),
+      ])
+    } else {
+      setNotes(notes => [currentNote, ...notes])
+    }
+  }
+
+  function getCurrentNoteIndex() {
+    return notes.findIndex(
+      ({ createdAt }) => createdAt === currentNote.createdAt
+    )
+  }
+
   return (
     <NoteContext.Provider
       value={{
+        notes,
         currentNote,
         editCurrentNote,
+        saveCurrentNote,
       }}
     >
       {children}
